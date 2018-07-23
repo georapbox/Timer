5280beeecab5f95ed353411ccbb40164ae492d32
Panagiotis Papadatis <hennett.afa@gmail.com>
2018-07-23 14:48:06 +0300

    add test functionality and update dependencies

---
+++ test/Timer.spec.js
@@ -0,0 +1,67 @@
+import Timer from '../src/index';
+
+const assert = require('assert');
+
+let timer,
+  result = 0;
+
+beforeEach(async () => {
+  window.requestAnimationFrame = callback => {
+    setTimeout(callback, 0);
+  };
+
+  timer = new Timer(1000, () => {});
+});
+
+describe('Timer Tests', () => {
+  it('initializes a timer', () => {
+    assert.ok(timer);
+  });
+
+  it('starts the timer withour reset', async () => {
+    await timer.start();
+    assert.ok(timer._started);
+  });
+
+  it('starts the timer and then resets', async () => {
+    await timer.start(true);
+    assert.ok(timer._started);
+
+    setTimeout(() => {
+      assert.ok(!timer._started);
+    }, 1100);
+  });
+
+  it('stops the timer', async () => {
+    await timer.start();
+    assert.ok(timer._started);
+
+    timer.stop();
+    assert.ok(!timer._started);
+  });
+
+  it('resets the timer without stop', async () => {
+    await timer.start();
+    assert.ok(timer._started);
+
+    timer.reset();
+    assert.ok(timer._started);
+  });
+
+  it('resets the timer and then stops', async () => {
+    await timer.start();
+    assert.ok(timer._started);
+
+    timer.reset(true);
+    assert.ok(!timer._started);
+  });
+
+  it('executes the callback function', async () => {
+    await timer.start();
+    assert.ok(timer._started);
+
+    setTimeout(() => {
+      assert.equal(result, 1);
+    }, 1100);
+  });
+});
